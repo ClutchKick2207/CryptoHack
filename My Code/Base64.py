327588214027827c6917e472554aba2daf2f5dcf
Ariz Ahmed <ariz.ahmed@wellard.net.au>
2022-02-06 22:15:11 +0800

    Update Base64.py

--- My Code/Base64.py
+++ My Code/Base64.py
@@ -1,3 +1,9 @@
-import base64
+from base64 import b64encode
 
 encoded = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
+
+test = bytearray.fromhex('72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf')
+
+output = b64encode(test)
+
+print(output)
